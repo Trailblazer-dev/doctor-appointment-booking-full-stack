39420ba7322f637416f5d98264e56f0c7ec0e270
Trailblazer-dev <richvictor830@gmail.com>
2024-09-26 22:54:18 +0300

    building

--- app/page.js
+++ app/page.js
@@ -1,11 +1,13 @@
 
 import Image from "next/image";
 import Hero from "./_components/Hero";
+import SearchBar from "./_components/SearchBar";
 
 export default function Home() {
   return (
     <div>
       <Hero/>
+      <SearchBar/>
     </div>
   );
 }
