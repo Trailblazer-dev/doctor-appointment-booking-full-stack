a428b37f3cc1b15550ca0f25ac46e528cfeea110
Trailblazer-dev <richvictor830@gmail.com>
2024-10-02 11:12:14 +0300

    building

--- app/_components/SearchBar.jsx
+++ app/_components/SearchBar.jsx
@@ -1,21 +1,47 @@
-import React from 'react'
-import { Button } from "@/components/ui/button"
-import { Input } from "@/components/ui/input"
-import { Search } from 'lucide-react'
+"use client";
+import React, { useEffect, useState } from "react";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Search } from "lucide-react";
+import GlobalApi from "../_utils/GlobalApi";
+import Image from "next/image";
 
-const SearchBar = () => {
+function SearchBar() {
+  const [categoryList, setCategoryList] = useState([]);
+  useEffect(() => {
+    getCategoryList();
+    // cleanup function
+  }, []);
+
+  const getCategoryList = () => {
+    GlobalApi.getCategory().then((resp) => {
+      console.log(resp.data.data);
+      setCategoryList(resp.data.data);
+    });
+  };
   return (
-    <div className='mb-10 items-center flex flex-col gap-2'>
-        <h2 className='font-bold text-4xl tracking-wide'>Search<span className='text-primary'> Doctors</span></h2>
-        <h2 className='text-gray-500 text-xl'>Search Your Doctor and Book Appointments in one click</h2>
-        <div className="flex w-full mt-3 max-w-sm items-center space-x-2">
-      <Input type="text" placeholder="Search..." />
-      <Button type="submit">
-        <Search className='h-4 w-4 mr-2'/>
-        Search</Button>
+    <div className="mb-10 items-center flex flex-col gap-2">
+      <h2 className="font-bold text-4xl tracking-wide">
+        Search<span className="text-primary"> Doctors</span>
+      </h2>
+      <h2 className="text-gray-500 text-xl">
+        Search Your Doctor and Book Appointments in one click
+      </h2>
+      <div className="flex w-full mt-3 max-w-sm items-center space-x-2">
+        <Input type="text" placeholder="Search..." />
+        <Button type="submit">
+          <Search className="h-4 w-4 mr-2" />
+          Search
+        </Button>
+      </div>
+      {/* Display list of category */}
+      {categoryList.map((item, index) => (
+        <div>
+          <Image src={item.Icon?.[0]?.url} alt="icon" width={40} height={40} />
+        </div>
+      ))}
     </div>
-    </div>
-  )
+  );
 }
 
-export default SearchBar
+export default SearchBar;
